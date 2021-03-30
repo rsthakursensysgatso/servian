6897b63d93d48e426569cdee3f75148a0f646002
rsthakur83 <rsthakur83@yahoo.com>
2021-03-30 11:49:04 +0200

    Add files via upload

---
+++ backend/backend.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region = var.aws_region
+}
+
+
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "app-terraform-state-bucket"
+  acl    = "private"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  tags = {
+    Name        = "Terraform State"
+  }
+}
