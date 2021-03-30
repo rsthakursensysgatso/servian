70fc53330beb05e460bb142e91a4cc6ba06c6e2f
rsthakur83 <rsthakur83@yahoo.com>
2021-03-30 17:31:36 +0200

    Update status.sh

--- statebucket/status.sh
+++ statebucket/status.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-cmd=`../terraform show |grep aws_alb|awk '{print $3}'|cut -d'"' -f 2`
+cmd=`/root/project/terraform show |grep aws_alb|awk '{print $3}'|cut -d'"' -f 2`
 res=`curl -s $cmd/healthcheck/`
 resp="OK"
 if [ $res == $resp ]
