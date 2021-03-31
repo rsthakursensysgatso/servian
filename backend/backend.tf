cd95a94e176a8c8b0c60c6e6e9c8f034bb97063a
rsthakur83 <rsthakur83@yahoo.com>
2021-03-31 12:24:59 +0200

    Update backend.tf

--- backend/backend.tf
+++ backend/backend.tf
@@ -22,3 +22,22 @@ resource "aws_s3_bucket" "terraform-state" {
     Name        = "Terraform State"
   }
 }
+
+resource "aws_s3_bucket" "artifact" {
+  bucket = "app_artifact_bucket"
+  acl    = "private"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  tags = {
+    Name        = "Artficat Bucket"
+  }
+}
