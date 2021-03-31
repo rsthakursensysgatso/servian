7fa55588991d50b4c7c015c73967fe51d15db332
rsthakur83 <rsthakur83@yahoo.com>
2021-03-31 17:46:53 +0200

    Update deploy.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -6,8 +6,8 @@ lc=`aws autoscaling describe-launch-configurations --region us-east-1|grep Launc
 if [[ "$lc" == "$lcfg1" ]]
 then
 	cd lc2
-	../terraform init
-	../terraform apply --auto-approve  
+	terraform init
+	terraform apply --auto-approve  
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name APP-ASG --launch-configuration-name $lcfg2 --min-size 4 --max-size 4
 	sleep 150
 	aws autoscaling delete-launch-configuration --launch-configuration-name $lcfg1
@@ -16,15 +16,15 @@ then
 elif [[ "$lc" == "$lcfg2" ]]
 then
 	cd lc1
-	../terraform init
-	../terraform apply --auto-approve  
+	terraform init
+	terraform apply --auto-approve  
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name APP-ASG --launch-configuration-name $lcfg1 1 --min-size 4 --max-size 4
 	sleep 150
 	aws autoscaling delete-launch-configuration --launch-configuration-name $lcfg2
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name APP-ASG --launch-configuration-name $lcfg1 --min-size 2 --max-size 2
 	
 else
-	../terraform init
-	../terraform plan
-	../terraform apply --auto-approve  
+	terraform init
+	terraform plan
+	terraform apply --auto-approve  
 fi
