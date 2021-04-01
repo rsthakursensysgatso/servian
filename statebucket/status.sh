d017ca18bdaf91b4f66c6b4b1b27cb0643a501ef
rsthakur83 <rsthakur83@yahoo.com>
2021-04-01 09:24:27 +0200

    Update status.sh

--- statebucket/status.sh
+++ statebucket/status.sh
@@ -3,7 +3,7 @@
 cmd=`/root/project/terraform show |grep aws_alb|awk '{print $3}'|cut -d'"' -f 2`
 res=`curl -s $cmd/healthcheck/`
 resp="OK"
-if [ $res == $resp ];
+if [[ $res == $resp ]]
 then
    echo "App Responding Ok!!!!!!"
 else
