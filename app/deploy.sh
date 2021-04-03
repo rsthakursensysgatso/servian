0852971439289bd2ea9fd716949e40baf86a089c
root <root@training-ravinder-4.c.puls-xilium.internal>
2021-04-03 12:04:16 +0000

    asg desired capacity

--- app/deploy.sh
+++ app/deploy.sh
@@ -12,7 +12,7 @@ if [ "$lc" == "$lcfg1" ];then
 	aws autoscaling create-launch-configuration --launch-configuration-name $lcfg2 --key-name serkey --image-id ami-042e8287309f5df03 --instance-type t2.micro --iam-instance-profile  cwdb_iam_profile --security-groups $sg  --user-data file:///root/project/app/deploy/userdata-asg.sh
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg2 --min-size 4 --max-size 4
 	sleep 150
-	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg2 --min-size 2 --max-size 3
+	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg2 --min-size 2 --max-size 3 --desired-capacity 2
 	sleep 60
 	aws autoscaling delete-launch-configuration --launch-configuration-name $lcfg1
 
@@ -24,7 +24,7 @@ elif [ "$lc" == "$lcfg2" ];then
 	aws autoscaling create-launch-configuration --launch-configuration-name $lcfg1 --key-name serkey --image-id ami-042e8287309f5df03 --instance-type t2.micro --iam-instance-profile  cwdb_iam_profile --security-groups $sg  --user-data file:///root/project/app/deploy/userdata-asg.sh
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg1 1 --min-size 4 --max-size 4
 	sleep 150
-	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg1 --min-size 2 --max-size 3
+	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg1 --min-size 2 --max-size 3 --desired-capacity 2
 	sleep 60
 	aws autoscaling delete-launch-configuration --launch-configuration-name $lcfg2
 	
