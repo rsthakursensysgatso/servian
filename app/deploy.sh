8ecb3f8c618db48108eb48e5f9b45a5e191748bf
root <root@training-ravinder-4.c.puls-xilium.internal>
2021-04-03 14:01:28 +0000

    deploy.sh with tag name sg

--- app/deploy.sh
+++ app/deploy.sh
@@ -8,7 +8,7 @@ lc=`aws autoscaling describe-launch-configurations --region aws-region|grep Laun
 if [ "$lc" == "$lcfg1" ];then
 
 	vpc=`aws ec2 describe-vpcs --filters Name=tag:Name,Values='Servian App VPC' --query 'Vpcs[*].VpcId' --output text`
-	sg=`aws ec2  describe-security-groups --filter Name=vpc-id,Values=$vpc  Name=group-name,Values=*APP*SG --region aws-region --query 'SecurityGroups[*].[GroupId]' --output text`
+	sg=`aws ec2  describe-security-groups --filter Name=vpc-id,Values=$vpc  Name=tag:Name,Values='APP SG' --region aws-region --query 'SecurityGroups[*].[GroupId]' --output text`
 	aws autoscaling create-launch-configuration --launch-configuration-name $lcfg2 --key-name serkey --image-id ami-042e8287309f5df03 --instance-type t2.micro --iam-instance-profile  cwdb_iam_profile --security-groups $sg  --user-data file:///root/project/app/deploy/userdata-asg.sh
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg2 --min-size 4 --max-size 4
 	sleep 150
@@ -20,7 +20,7 @@ if [ "$lc" == "$lcfg1" ];then
 elif [ "$lc" == "$lcfg2" ];then
 
 	vpc=`aws ec2 describe-vpcs --filters Name=tag:Name,Values='Servian App VPC' --query 'Vpcs[*].VpcId' --output text`
-	sg=`aws ec2  describe-security-groups --filter Name=vpc-id,Values=$vpc  Name=group-name,Values=*APP*SG --region aws-region --query 'SecurityGroups[*].[GroupId]' --output text`
+	sg=`aws ec2  describe-security-groups --filter Name=vpc-id,Values=$vpc  Name=tag:Name,Values='APP SG' --region aws-region --query 'SecurityGroups[*].[GroupId]' --output text`
 	aws autoscaling create-launch-configuration --launch-configuration-name $lcfg1 --key-name serkey --image-id ami-042e8287309f5df03 --instance-type t2.micro --iam-instance-profile  cwdb_iam_profile --security-groups $sg  --user-data file:///root/project/app/deploy/userdata-asg.sh
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg1 1 --min-size 4 --max-size 4
 	sleep 150
