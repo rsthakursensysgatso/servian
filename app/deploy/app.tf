4d4b0f91c474c1a22c5757dc215c0477655e8502
root <root@training-ravinder-4.c.puls-xilium.internal>
2021-04-03 10:40:05 +0000

    update asg tag

--- app/deploy/app.tf
+++ app/deploy/app.tf
@@ -183,7 +183,7 @@ resource "aws_subnet" "app_subnet_1" {
   vpc_id     = aws_vpc.app_vpc.id
   cidr_block = var.aws_app_subnet_1_cidr
   tags = {
-    Name = "APP subnet"
+    Name = "APP subnet 1"
   }
   availability_zone = var.az_zone_1
 }
@@ -193,7 +193,7 @@ resource "aws_subnet" "app_subnet_2" {
   vpc_id     = aws_vpc.app_vpc.id
   cidr_block = var.aws_app_subnet_2_cidr
   tags = {
-    Name = "APP subnet"
+    Name = "APP subnet 2"
   }
   availability_zone = var.az_zone_2
 }
@@ -270,7 +270,7 @@ resource "aws_subnet" "pub_subnet_1" {
   vpc_id     = aws_vpc.app_vpc.id
   cidr_block = var.aws_pub_subnet_1_cidr
   tags = {
-    Name = "public subnet"
+    Name = "public subnet 1"
   }
   availability_zone = var.az_zone_1
 }
